7ab40abdec29589c1909bd9d76ed0d4652b6bc46
AnnatarHe <annatar.he+github@gmail.com>
2024-03-04 22:40:30 +0800

    feat(test): add tests

---
+++ mocks/handlers.ts
@@ -0,0 +1,12 @@
+import { HttpResponse, http } from 'msw'
+
+// Define handlers that catch the corresponding requests and returns the mock data.
+export const handlers = [
+  http.post('http://127.0.0.1:8964/api/v1/public/prompts/run/PROMPT_ID', () => {
+    return HttpResponse.json({
+      id: 'PROMPT_ID',
+      message: 'good job! PromptPal JS SDK',
+      tokenCount: 888,
+    }, { status: 200 })
+  }),
+]
---
+++ src/client.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from 'vitest'
+import PromptPalClient from './client'
+
+test('client should execute correctly', async () => {
+  const c = new PromptPalClient('http://127.0.0.1:8964', 'token')
+  const res = await c.execute('PROMPT_ID', { text: 'hello world' }, '123')
+
+  expect(res.id).toBe('PROMPT_ID')
+  expect(res.message).toBe('good job! PromptPal JS SDK')
+  expect(res.tokenCount).toBe(888)
+})
