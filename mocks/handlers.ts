fba72e89128461416e807951603d3418fa56f084
AnnatarHe <annatar.he+github@gmail.com>
2024-07-09 02:28:01 +0800

    feat(streaming): add streaming api support

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,12 +1,57 @@
-import { HttpResponse, http } from 'msw'
+import { HttpResponse, http } from "msw";
+import { APIRunPromptResponse } from "../src/types";
+
+const encoder = new TextEncoder();
 
 // Define handlers that catch the corresponding requests and returns the mock data.
 export const handlers = [
-  http.post('http://127.0.0.1:7788/api/v1/public/prompts/run/PROMPT_ID', () => {
-    return HttpResponse.json({
-      id: 'PROMPT_ID',
-      message: 'good job! PromptPal JS SDK',
-      tokenCount: 888,
-    }, { status: 200 })
+  http.post("http://127.0.0.1:7788/api/v1/public/prompts/run/PROMPT_ID", () => {
+    return HttpResponse.json(
+      {
+        id: "PROMPT_ID",
+        message: "good job! PromptPal JS SDK",
+        tokenCount: 888,
+      },
+      { status: 200 },
+    );
   }),
-]
+  http.post(
+    "http://127.0.0.1:7788/api/v1/public/prompts/run/PROMPT_ID/stream",
+    () => {
+      const chunks: APIRunPromptResponse[] = [
+        {
+          id: "0",
+          message: "msg1",
+          tokenCount: -1,
+        },
+        {
+          id: "1",
+          message: "msg2",
+          tokenCount: -1,
+        },
+        {
+          id: "2",
+          message: "msg3",
+          tokenCount: -1,
+        },
+      ];
+
+      const stream = new ReadableStream({
+        start(controller) {
+          // Encode the string chunks using "TextEncoder".
+          controller.enqueue(encoder.encode(JSON.stringify(chunks[0])));
+          controller.enqueue(encoder.encode(JSON.stringify(chunks[1])));
+          controller.enqueue(encoder.encode(JSON.stringify(chunks[2])));
+          controller.close();
+        },
+      });
+
+      // Send the mocked response immediately.
+      return new HttpResponse(stream, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    },
+  ),
+];
--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,25 @@
 export type ErrorResponse = {
-  code: number
-  error: string
-}
+  code: number;
+  error: string;
+};
 
 export type APIRunPromptPayload<V extends object> = {
-  variables: V
-  userId?: string
-}
+  variables: V;
+  userId?: string;
+};
 
 export type APIRunPromptResponse = {
-  id: string
-  message: string
-  tokenCount: number
+  id: string;
+  message: string;
+  tokenCount: number;
+};
+
+export interface StreamEvents<C extends object> {
+  onData(chunk: C): Promise<C>;
+  onEnd(chunk?: C): Promise<C>;
 }
 
-/** 
+/**
  * the `promptpal.yaml` configuration type
  * ```ts
  * import { type Configuration } from "@prompt-pal/node-sdk"
@@ -25,44 +30,44 @@ export type APIRunPromptResponse = {
 export type Configuration = {
   input: {
     http?: {
-      url: string
-      token: string
-    }
-  }
+      url: string;
+      token: string;
+    };
+  };
   output: {
-    schema: string
+    schema: string;
     go_types?: {
-      prefix: string
-      package_name: string
-      output: string
-    }
+      prefix: string;
+      package_name: string;
+      output: string;
+    };
     typescript_types?: {
-      prefix: string
-      output: string
-    }
-  }
-}
+      prefix: string;
+      output: string;
+    };
+  };
+};
 
 export type RuntimeOptions = {
   /**
    * runtime cache for the PromptPal Node.js SDK
-   * 
+   *
    * @default Map struct
    * if `null`, the cache is disabled
    * you also can use any implementation that implements the `Cache` interface
    */
-  cache?: Cache | null
-  defaultTimeout?: number
-}
+  cache?: Cache | null;
+  defaultTimeout?: number;
+};
 
 /**
  * cache interface
  * default is a `Map`
  */
 export interface Cache {
-  get(key: string): string | undefined
-  set(key: string, value: string): void
-  has(key: string): boolean
-  delete(key: string): void
-  clear(): void
+  get(key: string): string | undefined;
+  set(key: string, value: string): void;
+  has(key: string): boolean;
+  delete(key: string): void;
+  clear(): void;
 }
